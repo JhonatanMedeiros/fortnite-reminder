f6bc0bd64c37037857aa2047a165737a5be98204
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2022-09-06 00:40:48 -0300

    feat: changed cron job

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,13 @@ import TelegramAlert from './telegram';
 
 import { hasItemInShop } from './reminder';
 
+const CRON_JOB = '0 21 * * *';
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
 const start = async () => {
 	try {
+		console.log('Getting Fortnite Shop...')
 		const response = await FortniteAPI.shop();
 		const { data } = response;
 		const items = getItemsFromShop(data);
@@ -26,9 +31,24 @@ const start = async () => {
 }
 
 console.log('Job initialized');
-const job = new cron.CronJob('1 9 * * *', async () => {
+const job = new cron.CronJob(CRON_JOB, async () => {
+	console.log('Starting job...');
+
+	const startJobDate = new Date();
+	console.log('Started job:', startJobDate);
+
+	await sleep(60000); // await 1 minute
+
+	// Send message to bot
 	await TelegramAlert.sendMessage('🔍 Checking for items in the shop...');
+
+	// Start Fortnite API
 	await start();
+
+	const endJobDate = new Date();
+	console.log('Ended Job:', endJobDate);
 });
 
 job.start();
+
+console.log('is job running? ', job.running);
